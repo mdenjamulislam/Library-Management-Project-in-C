608a42f6a99685eb929f115c1376d25238be4413
Md Enjamul Islam <mdenjamulislam527@gmail.com>
2024-07-02 13:55:49 +0600

    Update

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-typedef struct {
+typedef struct
+{
     int id;
     char title[100];
     char author[100];
@@ -19,11 +19,12 @@ void updateBook();
 void deleteBook();
 int getNextId();
 
-
-int main() {
+int main()
+{
     int choice;
 
-    while (1) {
+    while (1)
+    {
         printf("\nLibrary Management System\n");
         printf("1. Add Book\n");
         printf("2. View Books\n");
@@ -34,35 +35,37 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                addBook();
-                break;
-            case 2:
-                viewBooks();
-                break;
-            case 3:
-                searchBook();
-                break;
-            case 4:
-                deleteBook();
-                break;
-            case 5:
-                updateBook();
-                break;
-            case 6:
-                exit(0);
-            default:
-                printf("Invalid choice! Please try again.\n");
+        switch (choice)
+        {
+        case 1:
+            addBook();
+            break;
+        case 2:
+            viewBooks();
+            break;
+        case 3:
+            searchBook();
+            break;
+        case 4:
+            deleteBook();
+            break;
+        case 5:
+            updateBook();
+            break;
+        case 6:
+            exit(0);
+        default:
+            printf("Invalid choice! Please try again.\n");
         }
     }
     return 0;
 }
 
-
-void addBook() {
+void addBook()
+{
     FILE *file = fopen("text.txt", "a");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file!\n");
         return;
     }
@@ -72,12 +75,12 @@ void addBook() {
 
     printf("Enter book title: ");
     getchar(); // Clear newline character from buffer
-    fgets(book.title, MAX_TITLE_LENGTH, stdin);
-        // Remove newline character from book title
+    fgets(book.title, 100, stdin);
+    // Remove newline character from book title
     book.title[strcspn(book.title, "\n")] = '\0';
 
     printf("Enter author name: ");
-    fgets(book.author, MAX_AUTHOR_LENGTH, stdin);
+    fgets(book.author, 100, stdin);
     book.author[strcspn(book.author, "\n")] = '\0';
 
     printf("Enter publication year: ");
@@ -89,17 +92,20 @@ void addBook() {
     printf("Book added successfully!\n");
 }
 
-
-int getNextId() {
+int getNextId()
+{
     FILE *file = fopen("text.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         return 1; // Starting ID
     }
 
     int id = 0;
     Book book;
-    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF) {
-        if (book.id > id) {
+    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF)
+    {
+        if (book.id > id)
+        {
             id = book.id;
         }
     }
@@ -107,69 +113,77 @@ int getNextId() {
     return id + 1;
 }
 
-
-void viewBooks() {
+void viewBooks()
+{
     FILE *file = fopen("text.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file!\n");
         return;
     }
 
     printf("\nBook List:\n");
     printf("ID\tTitle\t\tAuthor\t\tYear\n");
-    printf("---------------------------------------------\n");
+    printf("-----------------------------------------------------------------\n");
 
     Book book;
-    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF) {
-        printf("%d\t%s\t\t%s\t\t%d\n", book.id, book.title, book.author, book.year);
+    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF)
+    {
+        printf("%d\t%s\t\t%s\t%d\n", book.id, book.title, book.author, book.year);
     }
     fclose(file);
 }
 
-
-void searchBook() {
-    char title[MAX_TITLE_LENGTH];
+void searchBook()
+{
+    char title[100];
     printf("Enter the book title to search: ");
     getchar(); // Clear newline character from buffer
-    fgets(title, MAX_TITLE_LENGTH, stdin);
+    fgets(title, 100, stdin);
     title[strcspn(title, "\n")] = '\0';
 
     FILE *file = fopen("text.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file!\n");
         return;
     }
 
     Book book;
     int found = 0;
-    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF) {
-        if (strcasecmp(book.title, title) == 0) {
+    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF)
+    {
+        if (strcasecmp(book.title, title) == 0)
+        {
             printf("Book found:\n");
             printf("ID: %d\nTitle: %s\nAuthor: %s\nYear: %d\n", book.id, book.title, book.author, book.year);
             found = 1;
             break;
         }
     }
-    if (!found) {
+    if (!found)
+    {
         printf("Book not found!\n");
     }
     fclose(file);
 }
 
-
-void updateBook() {
+void updateBook()
+{
     int id;
     printf("Enter the book ID to update: ");
     scanf("%d", &id);
 
     FILE *file = fopen("text.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file!\n");
         return;
     }
 
     FILE *tempFile = fopen("temp.txt", "w");
-    if (tempFile == NULL) {
+    if (tempFile == NULL)
+    {
         printf("Error opening temporary file!\n");
         fclose(file);
         return;
@@ -177,16 +191,18 @@ void updateBook() {
 
     Book book;
     int found = 0;
-    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF) {
-        if (book.id == id) {
+    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF)
+    {
+        if (book.id == id)
+        {
             found = 1;
             printf("Enter new title: ");
             getchar(); // Clear newline character from buffer
-            fgets(book.title, MAX_TITLE_LENGTH, stdin);
+            fgets(book.title, 100, stdin);
             book.title[strcspn(book.title, "\n")] = '\0';
 
             printf("Enter new author: ");
-            fgets(book.author, MAX_AUTHOR_LENGTH, stdin);
+            fgets(book.author, 100, stdin);
             book.author[strcspn(book.author, "\n")] = '\0';
 
             printf("Enter new publication year: ");
@@ -197,31 +213,35 @@ void updateBook() {
     fclose(file);
     fclose(tempFile);
 
-    if (found) {
+    if (found)
+    {
         remove("text.txt");
         rename("temp.txt", "text.txt");
         printf("Book updated successfully!\n");
-    } else {
+    }
+    else
+    {
         remove("temp.txt");
         printf("Book not found!\n");
     }
 }
-1
 
-
-void deleteBook() {
+void deleteBook()
+{
     int id;
     printf("Enter the book ID to delete: ");
     scanf("%d", &id);
 
     FILE *file = fopen("text.txt", "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file!\n");
         return;
     }
 
     FILE *tempFile = fopen("temp.txt", "w");
-    if (tempFile == NULL) {
+    if (tempFile == NULL)
+    {
         printf("Error opening temporary file!\n");
         fclose(file);
         return;
@@ -229,25 +249,29 @@ void deleteBook() {
 
     Book book;
     int found = 0;
-    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF) {
-        if (book.id == id) {
+    while (fscanf(file, "%d,%[^,],%[^,],%d\n", &book.id, book.title, book.author, &book.year) != EOF)
+    {
+        if (book.id == id)
+        {
             found = 1;
-        } else {
+        }
+        else
+        {
             fprintf(tempFile, "%d,%s,%s,%d\n", book.id, book.title, book.author, book.year);
         }
     }
     fclose(file);
     fclose(tempFile);
 
-    if (found) {
+    if (found)
+    {
         remove("text.txt");
         rename("temp.txt", "text.txt");
         printf("Book deleted successfully!\n");
-    } else {
+    }
+    else
+    {
         remove("temp.txt");
         printf("Book not found!\n");
     }
 }
-
-
-
